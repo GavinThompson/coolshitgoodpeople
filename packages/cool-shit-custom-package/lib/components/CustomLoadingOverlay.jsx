52ea7efe51d3f14fd49fe05f69f3d1c99757a689
Gavin Thompson <gavinathompson@gmail.com>
2016-12-10 10:54:04 -0800

    pseudo-fixed loading; issue is was that buffer bars were being added after dom loads in the material design App.js file -- one of the last files loaded into the app. I've added the extra bars manually to the CustomOverlay component. Realisitically I should probably just copy the style for the entire section and rename the classes -- if there is a render problem we'll get a second load problem as it is

--- packages/cool-shit-custom-package/lib/components/CustomLoadingOverlay.jsx
+++ packages/cool-shit-custom-package/lib/components/CustomLoadingOverlay.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+let width0 = { width: "0%" }
+let width100 = { width: "100%" }
+
 const CustomLoadingOverlay = ({color}) => {
   return (
     <div className="loading-overlay" id="appLoad">
       <div className="center">
-        <div className="mdl-progress mdl-js-progress mdl-progress__indeterminate"></div>
+        <div className="mdl-progress mdl-js-progress mdl-progress__indeterminate">
+          
+          <div className="progressbar bar bar1" style={width0}></div>
+          <div className="bufferbar bar bar2" style={width100}></div>
+          <div className="auxbar bar bar3" style={width0}></div>
+
+        </div>
       </div>
     </div>
   )
